22a5bc35e789526bb1708c29fb289c74a8344f73
tangle <tangle@zhinantech.com>
2022-01-19 01:25:05 +0800

    update

--- src/main/java/com/lee/hof/sys/bean/model/BaseInput.java
+++ src/main/java/com/lee/hof/sys/bean/model/BaseInput.java
@@ -12,18 +12,19 @@ import javax.validation.constraints.NotNull;
 
 public class BaseInput {
 
-    @NotNull
-    private String userId;
-
-    private User user;
-
-    public String getUserId() {
+    public Long getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(Long userId) {
         this.userId = userId;
     }
+
+    @NotNull
+    private Long userId;
+
+    private User user;
+
 
     public User getUser() {
         return user;
