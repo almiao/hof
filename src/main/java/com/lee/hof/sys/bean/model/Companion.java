549537cbd46f5215258bf007292eae65a3b75f2c
almiao <foreverlee111@gmail.com>
2023-04-09 02:16:17 +0800

    update

--- src/main/java/com/lee/hof/sys/bean/model/Companion.java
+++ src/main/java/com/lee/hof/sys/bean/model/Companion.java
@@ -30,27 +30,88 @@ public class Companion implements Serializable {
     private Long id;
 
 
+    /**
+     * 如4月下旬
+     */
     private String title;
 
+    /**
+     * 作者描述
+     */
     private String contentDesc;
 
+    /**
+     * 图片
+     */
     private String picIds;
 
-    private String startLocation;
+    /**
+     * 出发城市
+     */
+    private String startCity;
 
-    private String startDetailLocation;
+    /**
+     * 出发地点描述
+     */
+    private String startDetailLocationDesc;
 
+    /**
+     * 出发具体坐标
+     */
+    private String startDetailLocationPoi;
+
+
+    /**
+     * 到达城市
+     */
     private String endCity;
 
+    /**
+     * 具体景点描述
+     */
+    private String endDetailLocationDesc;
+
+    /**
+     * 具体经典坐标
+     */
+    private String endDetailLocationPoi;
+
+    /**
+     * 活动类型
+     */
     private String companionType;
 
+    /**
+     * 预期同行人数
+     */
     private int expectPersonNum;
 
+    /**
+     * 预期开始日期
+     */
     private String expectStartDate;
 
+    /**
+     * 预期结束日志
+     */
     private String expectEndDate;
 
+    /**
+     *
+     */
     private String referenceCompanionId;
+
+    /**
+     * 费用预期
+     */
+    private String maxCostDesc;
+
+
+    /**
+     * 费用
+     */
+    private String costShareType;
+
 
     @TableField(fill = FieldFill.INSERT)
     private Long createBy;
